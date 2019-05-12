42bfde8b212caabf97a1fe23a90577adc1b5007f
顺子 <shunzi@shunzideMacBook-Air.local>
2019-05-12 17:41:33 +0800

    2019.5.12.3

--- .klei/DoNotStarveTogether/MyDediServer/Master/modoverrides.lua
+++ .klei/DoNotStarveTogether/MyDediServer/Master/modoverrides.lua
@@ -1,3 +1,7 @@
 return {
-  ["workshop-375859599"]={ configuration_options={  }, enabled=true }
+  ["workshop-786556008"]={ configuration_options={  }, enabled=true },
+  ["workshop-661253977"]={ configuration_options={  }, enabled=true },
+  ["workshop-463292274"]={ configuration_options={  }, enabled=true },
+  ["workshop-959771965"]={ configuration_options={  }, enabled=true },
+  ["workshop-378160973"]={ configuration_options={  }, enabled=true },
 }
